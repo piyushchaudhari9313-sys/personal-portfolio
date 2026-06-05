b45e737426591863a42549c5b4be076151acbd98
Piyush Chaudhari <piyushchaudhari9313@gmail.com>
2026-06-05 10:37:29 +0530

    Add files via upload

--- projects/todo app/script.js
+++ projects/todo app/script.js
@@ -1,5 +1,23 @@
 let inputs=document.querySelector("#inp");
 let text=document.querySelector(".text");
+let value=[];
+let savevalue=JSON.parse(localStorage.getItem("values"))
+
+if(savevalue){
+    value=savevalue;
+}
+value.forEach((Data,index) => {
+    let newel=document.createElement("ul");
+    newel.innerHTML=Data+"<i class='fa-solid fa-trash'></i>";
+    text.appendChild(newel);
+    newel.querySelector("i").addEventListener("click",remove);
+
+    function remove(){
+        newel.remove()
+        value.splice(index,1);
+        localStorage.setItem("values",JSON.stringify(value))
+    }
+}); 
 
 function Add(){
     if(inputs.value===""){
@@ -7,13 +25,19 @@ function Add(){
         alert("Please Enter Task");
     }
     else{
+        let Task=inputs.value;
+        value.push(Task);
+        localStorage.setItem("values",JSON.stringify(value));
         let newel=document.createElement("ul");
-        newel.innerHTML=inputs.value+"<i class='fa-solid fa-trash'></i>"
+        newel.innerHTML=Task+"<i class='fa-solid fa-trash'></i>"
         text.appendChild(newel);
-        inputs.value="";
+        inputs.value="";    
         newel.querySelector("i").addEventListener("click",remove);
+        
         function remove(){
             newel.remove()
+            value.splice(value.indexOf(Task),1)
+            localStorage.setItem("values",JSON.stringify(value));
         }
     }
 }
